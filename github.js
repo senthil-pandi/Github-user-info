39a16c69f844e99907b3a00ffce45aade10d78c7
Senthil Pandi <80332839+senthil-pandi@users.noreply.github.com>
2021-10-19 12:44:26 +0530

    Add files via upload

---
+++ github.js
@@ -0,0 +1,16 @@
+class Github {
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}`);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
