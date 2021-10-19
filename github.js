2e69f65605e6f330537f217f37bb9db11a19240b
Senthil Pandi <80332839+senthil-pandi@users.noreply.github.com>
2021-10-19 12:58:42 +0530

    Update github.js

--- github.js
+++ github.js
@@ -1,14 +1,16 @@
 class Github {
     constructor() {
-
+        this.client_id = 'your github app client id';
+        this.client_secret = 'your github app secret key';
+        //you can get the aboove details by creating a new app in https://github.com/settings/applications/new
         this.repos_count = 5;
         this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
-        const profileResponse = await fetch(`https://api.github.com/users/${user}`);
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
-        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}`);
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
         const profile = await profileResponse.json();
         const repos = await repoResponse.json();
